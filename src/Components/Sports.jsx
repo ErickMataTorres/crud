8f96217e7760d5d321aa19108125ab7c47144cf0
Erick Mata Torres <abcdefghij110@outlook.com>
2023-11-01 12:09:18 -0700

    Agregando funcionalidad al boton para guardar un registro

--- src/Components/Sports.jsx
+++ src/Components/Sports.jsx
@@ -1,15 +1,15 @@
-import  {useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import "../Styles/Sports.css";
 import API_URL from "../API_URL.JS";
 
 function Sports() {
     const [data, setData] = useState([]);
-    const [id, setId]=useState("");
-    const [name,setName]=useState("");
-    
+    const [id, setId] = useState("");
+    const [name, setName] = useState("");
 
     useEffect(() => {
-        fetch(API_URL + "/Sports/getSports?parameter=''")
+        const parameter="";
+        fetch(API_URL + `/Sports/GetSports?parameter=${[parameter]}`)
             .then((response) => {
                 if (!response.ok) {
                     throw new Error("Network response was not ok");
@@ -18,23 +18,43 @@ function Sports() {
             })
             .then((data) => {
                 setData(data);
-
             })
             .catch((error) => {
                 console.error("Error get the data: ", error);
             });
     }, []);
 
-    const OpenModal=(action, item)=>{
-        if(action==="New"){
+    const OpenModal = (action, item) => {
+        if (action === "New") {
+            document.getElementById("exampleModalLabel").innerText=action;
             setId("New");
             setName("");
-        }else{
-            if(action==="Modify"){
+        } else {
+            if (action === "Modify") {
+                document.getElementById("exampleModalLabel").innerText=action;
                 setId(item.Id);
                 setName(item.Name);
             }
         }
+    }
+
+    const ClickAction=()=>{
+        fetch(API_URL+"/Sports/SaveSport",{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json"
+            },
+            body:JSON.stringify({Id: id , Name: name})
+        })
+        .then(response => response.json())
+        .then(response=>{
+            console.log(response);
+            const newRegister={Id:response, Name:name};
+            setData([...data, newRegister]);
+        })
+        .catch(error=>{
+            console.error("Ha ocurrido un error: ", error);
+        });
     }
 
 
@@ -47,7 +67,7 @@ function Sports() {
                 <div className="searchContainer">
                     <label>Search:</label>
                     <input type="text" placeholder="..." className="form-control" />
-                    <button className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal" onClick={()=>OpenModal("New","")}>New</button>
+                    <button className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal" onClick={() => OpenModal("New", "")}>New</button>
                 </div>
 
                 <table className="table table-hover">
@@ -64,7 +84,7 @@ function Sports() {
                                 <th scope="row">{item.Id}</th>
                                 <td>{item.Name}</td>
                                 <td>
-                                    <button className="btn btn-warning" data-bs-toggle="modal" data-bs-target="#myModal" onClick={()=>OpenModal("Modify",item)}>Modify</button>
+                                    <button className="btn btn-warning" data-bs-toggle="modal" data-bs-target="#myModal" onClick={() => OpenModal("Modify", item)}>Modify</button>
                                     <button className="btn btn-danger">Delete</button>
                                 </td>
                             </tr>
@@ -79,22 +99,22 @@ function Sports() {
                 <div className="modal-dialog modal-dialog-centered">
                     <div className="modal-content">
                         <div className="modal-header">
-                            <h1 className="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                            <h1 className="modal-title fs-5" id="exampleModalLabel">Modal Title</h1>
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div className="modal-body">
                             <div className="modalInputsContainer">
                                 <label className="col-md-2">Id:</label>
-                                <input type="text" placeholder="..." className="form-control" id="inputId" value={id} readOnly/>
+                                <input type="text" placeholder="..." className="form-control" id="inputId" value={id} readOnly />
                             </div>
                             <div className="modalInputsContainer">
                                 <label className="col-md-2">Name:</label>
-                                <input type="text" placeholder="..." className="form-control" id="inputName" value={name} onChange={(event)=>setName(event.target.value)}/>
+                                <input type="text" placeholder="..." className="form-control" id="inputName" value={name} onChange={(event) => setName(event.target.value)} />
                             </div>
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" className="btn btn-primary">Save changes</button>
+                            <button type="button" className="btn btn-primary" onClick={ClickAction}>Save changes</button>
                         </div>
                     </div>
                 </div>
